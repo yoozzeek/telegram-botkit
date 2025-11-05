beec352c473a175c844c83a88580271d532de289
Andrew <zeek@tuta.com>
2025-11-05 10:18:45 +0700

    chore: use dtolnay/rust-toolchain v1.89.0 instead of stable to pass clippy check

--- src/viewport/store.rs
+++ src/viewport/store.rs
@@ -1,9 +1,10 @@
+use crate::viewport::MessageMeta;
 use teloxide::types::ChatId;
 
 #[async_trait::async_trait]
 pub trait Store: Send + Sync + 'static {
-    async fn save(&self, chat: ChatId, mid: i32, meta: super::MessageMeta) -> anyhow::Result<()>;
-    async fn load(&self, chat: ChatId, mid: i32) -> anyhow::Result<Option<super::MessageMeta>>;
+    async fn save(&self, chat: ChatId, mid: i32, meta: MessageMeta) -> anyhow::Result<()>;
+    async fn load(&self, chat: ChatId, mid: i32) -> anyhow::Result<Option<MessageMeta>>;
 }
 
 #[derive(Clone, Copy, Default)]
@@ -11,16 +12,11 @@ pub struct NoopStore;
 
 #[async_trait::async_trait]
 impl Store for NoopStore {
-    async fn save(
-        &self,
-        _chat: ChatId,
-        _mid: i32,
-        _meta: super::MessageMeta,
-    ) -> anyhow::Result<()> {
+    async fn save(&self, _chat: ChatId, _mid: i32, _meta: MessageMeta) -> anyhow::Result<()> {
         Ok(())
     }
 
-    async fn load(&self, _chat: ChatId, _mid: i32) -> anyhow::Result<Option<super::MessageMeta>> {
+    async fn load(&self, _chat: ChatId, _mid: i32) -> anyhow::Result<Option<MessageMeta>> {
         Ok(None)
     }
 }
